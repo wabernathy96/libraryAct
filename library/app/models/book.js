98b316a07188af86b02492ffc90ec94976c0b66a
Connor Thomas <connor.ian.thomas@gmail.com>
2018-05-03 21:03:16 -0500

    added modeling

--- library/app/models/book.js
+++ library/app/models/book.js
@@ -1,17 +1,27 @@
-// Dependencies
-// =============================================================
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
 
-// Require the sequelize library
-// Require the connection to the database (connection.js)
+// Creates a "Chirp" model that matches up with DB
+var Book = sequelize.define("book", {
+  title: {
+    type: Sequelize.STRING
+  },
+  author: {
+    type: Sequelize.STRING
+  },
+  genre: {
+    type: Sequelize.STRING
+  },
+  pages: {
+    type: Sequelize.INTEGER
+  }
+}, {
+  timestamps: false
+});
 
-// Create a "Book" model with the following configuration
+// Syncs with DB
+Book.sync();
 
-// 1. A title property of type STRING
-// 2. An author property of type STRING
-// 3. A genre property of type STRING
-// 4. A pages property of type INTEGER
-// 5. Set timestamps to false on this model
-
-// Sync model with DB
-
-// Export the book model for other files to use
+// Makes the Chirp Model available for other files (will also create a table)
+module.exports = Book;
